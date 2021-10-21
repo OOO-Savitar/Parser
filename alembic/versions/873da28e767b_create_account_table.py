efc50df1f0538863db23ffaaa01a6743d7fa267f
Petr <ppamuzhakk@mail.ru>
2021-10-22 00:27:56 +0300

    новая база

---
+++ alembic/versions/873da28e767b_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 873da28e767b
+Revises: 2b316727bd56
+Create Date: 2021-10-21 23:59:32.437165
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '873da28e767b'
+down_revision = '2b316727bd56'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
